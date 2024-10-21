972773aa54bce2bd479a6aab358ce9ba015c8b70
Andrey <getboros@gmail.com>
2024-10-21 17:37:41 +0300

    add new module, backup, preparing to refactoring source folder

---
+++ Source/Module_Menu_Main/Module_Menu_Main.Build.cs
@@ -0,0 +1,11 @@
+using UnrealBuildTool;
+
+public class Module_Menu_Main : ModuleRules
+{
+	public Module_Menu_Main(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/Module_Menu_Main/Private/JamScream.cpp
@@ -0,0 +1,4 @@
+#include "Modules/ModuleManager.h"
+
+// def immplement  makes available to c++ module
+IMPLEMENT_MODULE(FDefaultModuleImpl, MenuMainModule);
